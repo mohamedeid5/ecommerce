7b12f1668c1e839fd847b7694777b1f6d42aa91f
mohamed eid <meid368@gmail.com>
2021-07-04 18:53:50 +0200

    product

---
+++ resources/views/dashboard/products/general/index.blade.php
@@ -0,0 +1,45 @@
+@extends('dashboard.layouts.admin')
+
+@section('title', 'Products')
+
+@section('content')
+
+    <h2>Products</h2>
+
+    <a href="{{ route('admin.products.create') }}" class="btn btn-info">Add Product</a>
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">is active</th>
+            <th scope="col">created at</th>
+            <th scope="col">updated at</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($products as $product)
+          <tr>
+            <th scope="row">{{ $product->id }}</th>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->getActive() }}</td>
+            <td>{{ $product->created_at }}</td>
+            <td>{{ $product->updated_at }}</td>
+            <td>
+                <a href="{{ route('admin.products.edit', $product->id) }}" class="btn btn-primary">edit</a>
+                <form action="{{ route('admin.products.destroy', $product->id) }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger">delete</button>
+                </form>
+            </td>
+          </tr>
+          @endforeach
+
+        </tbody>
+      </table>
+      {!! $products->links() !!}
+
+@endsection
